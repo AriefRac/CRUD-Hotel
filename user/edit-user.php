a84ccb46c063d8ac2fcc4dd1ed1e7a5e8a049411
Arief <muhariefrac@gmail.com>
2025-12-01 15:12:01 +0700

    push CRUD room types

--- user/edit-user.php
+++ user/edit-user.php
@@ -43,7 +43,6 @@ if (isset($_POST['update'])) {
 
         <div class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
             <form action="" method="post">
-                <input type="text" name="id" value="<?= $user['user_id'] ?>" hidden>
                 <div class="flex justify-between items-center px-5 py-4 sm:px-6 sm:py-5">
                     <h3 class="text-xl font-medium text-gray-800 dark:text-white/90">
                         Edit Data User
@@ -58,6 +57,13 @@ if (isset($_POST['update'])) {
 
                 </div>
                 <div class="space-y-6 border-t border-gray-100 p-5 sm:p-6 dark:border-gray-800">
+                    <div>
+                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                            Id<span class="text-error-500">*</span>
+                        </label>
+                        <input type="text" name="id" value="<?= $user['user_id'] ?>" readonly
+                            class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90" />
+                    </div>
                     <div>
                         <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
                             Username<span class="text-error-500">*</span>
