a699d8487a5eb3ba19322476cc59b9661ac594e1
Arief <muhariefrac@gmail.com>
2025-12-03 20:23:44 +0700

    fix form room types and rooms

--- rooms/form-rooms.php
+++ rooms/form-rooms.php
@@ -134,18 +134,15 @@ if (isset($_POST['simpan'])) {
 
                 </div>
                 <div class="space-y-6 border-t border-gray-100 p-5 sm:p-6 dark:border-gray-800">
-                    <div>
-                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
-                            Id<span class="text-error-500">*</span>
-                        </label>
-                        <input type="text" name="id" value="<?= $msg != '' ? $room['id'] : null ?>" readonly
-                            class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90" />
-                    </div>
+
+                    <input type="text" name="id" value="<?= $msg != '' ? $room['id'] : null ?>" hidden />
+
                     <div>
                         <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
                             Room Number<span class="text-error-500">*</span>
                         </label>
-                        <input type="text" name="roomNumber" value="<?= $msg != '' ? $room['room_number'] : null ?>" required
+                        <input type="text" name="roomNumber" value="<?= $msg != '' ? $room['room_number'] : null ?>"
+                            required
                             class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30" />
                     </div>
                     <div>
@@ -160,8 +157,9 @@ if (isset($_POST['simpan'])) {
                                 <?php
                                 $roomTypes = getData("SELECT * FROM room_types");
                                 foreach ($roomTypes as $rt) { ?>
-                                    <option value="<?= $rt['id']?>" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
-                                        <?= $rt['id']?> | <?= $rt['name']?>
+                                    <option value="<?= $rt['id'] ?>"
+                                        class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                        <?= $rt['id'] ?> | <?= $rt['name'] ?>
                                     </option>
                                 <?php } ?>
                             </select>
@@ -184,15 +182,15 @@ if (isset($_POST['simpan'])) {
                                 class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pr-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
                                 :class="isOptionSelected && 'text-gray-800 dark:text-white/90'"
                                 @change="isOptionSelected = true">
-                                    <option value="available" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
-                                        Available
-                                    </option>
-                                    <option value="occupied" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
-                                        Occupied
-                                    </option>
-                                    <option value="maintenance" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
-                                        Maintenance
-                                    </option>
+                                <option value="available" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                    Available
+                                </option>
+                                <option value="occupied" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                    Occupied
+                                </option>
+                                <option value="maintenance" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                    Maintenance
+                                </option>
                             </select>
                             <span
                                 class="pointer-events-none absolute top-1/2 right-4 z-30 -translate-y-1/2 text-gray-500 dark:text-gray-400">
@@ -204,7 +202,7 @@ if (isset($_POST['simpan'])) {
                             </span>
                         </div>
                     </div>
-                    
+
                 </div>
             </form>
         </div>
